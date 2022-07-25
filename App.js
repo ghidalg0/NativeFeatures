6a088a3380ff7e4ebc2bcbd0697a728141e659f1
Guilhem Hidalgo <guilhem.hidalgo@tetraktys-cm.com>
2022-07-25 14:48:24 +0200

    added IconButton in the header

--- App.js
+++ App.js
@@ -6,6 +6,7 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 import { AllPlaces } from './screens/AllPlaces';
 import { AddPlace } from './screens/AddPlace';
+import { IconButton } from './components/ui/IconButton';
 
 const Stack = createNativeStackNavigator();
 
@@ -15,7 +16,13 @@ export default function App() {
       <StatusBar style="dark"/>
       <NavigationContainer>
         <Stack.Navigator>
-          <Stack.Screen name="AllPlaces" component={AllPlaces} />
+          <Stack.Screen
+            name="AllPlaces"
+            component={AllPlaces}
+            options={({navigation}) => ({
+              headerRight: ({tintColor}) => <IconButton icon="add" size={24} color={tintColor} onPress={() => navigation.navigate("AddPlace")} />
+            })}
+          />
           <Stack.Screen name="AddPlace" component={AddPlace} />
         </Stack.Navigator>
       </NavigationContainer>
