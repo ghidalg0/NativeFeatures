fac582390f90a767435716b4e0ec6a7e53bb204e
Guilhem Hidalgo <guilhem.hidalgo@tetraktys-cm.com>
2022-07-25 18:23:53 +0200

    added functions to confirm picked location

--- screens/Map.js
+++ screens/Map.js
@@ -1,8 +1,9 @@
-import { useState } from "react";
-import { StyleSheet } from "react-native";
+import { useCallback, useLayoutEffect, useState } from "react";
+import { Alert, StyleSheet } from "react-native";
 import MapView, { Marker } from "react-native-maps";
+import { IconButton } from "../components/ui/IconButton";
 
-export const Map = () => {
+export const Map = ({navigation}) => {
 
   const [selectedLocation, setSelectedLocation] = useState();
 
@@ -19,6 +20,20 @@ export const Map = () => {
 
     setSelectedLocation({lat: lat, lng: lng});
   };
+
+  const savePickedLocationHandler = useCallback(() => {
+    if (!selectedLocation) {
+      Alert.alert("No Location Picked", "You have to pick a location by taping on the map first...")
+      return;
+    }
+    navigation.navigate("AddPlace", {pickedLat: selectedLocation.lat, pickedLng: selectedLocation.lng});
+  }, [navigation, selectedLocation]);
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerRight: ({tintColor}) => <IconButton icon="save" size={24} color={tintColor} onPress={savePickedLocationHandler} />
+    });
+  }, [navigation, savePickedLocationHandler]);
 
   return (
     <MapView initialRegion={region} style={styles.map} onPress={selectLocationHandler} >
