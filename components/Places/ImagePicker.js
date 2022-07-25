24cb9211299c4ec306556773718219cf7b4066e3
Guilhem Hidalgo <guilhem.hidalgo@tetraktys-cm.com>
2022-07-25 16:03:42 +0200

    managed permission for IOS - working now for IOS

--- components/Places/ImagePicker.js
+++ components/Places/ImagePicker.js
@@ -1,9 +1,30 @@
-import { Button, StyleSheet, View } from "react-native";
-import { launchCameraAsync } from 'expo-image-picker'
+import { Alert, Button, StyleSheet, View } from "react-native";
+import { launchCameraAsync, useCameraPermissions, PermissionStatus } from 'expo-image-picker'
 
 export const ImagePicker = () => {
 
+  const [camPermissionInfo, requestPermission] = useCameraPermissions();
+
+  const verifyPermissions = async () => {
+    if (camPermissionInfo.status === PermissionStatus.UNDETERMINED) {
+      const permissionResponse = await requestPermission();
+      return permissionResponse.granted; // boolean
+    }
+    if (camPermissionInfo.status === PermissionStatus.DENIED) {
+      Alert.alert("Camera Blocked", "You did not granted access to camera");
+      return false;
+    }
+    return true;
+  };
+
   const takeImageHandler = async () => {
+
+    const hasPermission = await verifyPermissions();
+
+    if (!hasPermission) {
+      return;
+    }
+
     const image = await launchCameraAsync({
       allowsEditing: true,
       aspect: [16,9],
