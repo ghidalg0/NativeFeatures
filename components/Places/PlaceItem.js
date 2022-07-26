26a3edd7d80cff17378d2c71c0b9aa22097b1d7d
Guilhem Hidalgo <guilhem.hidalgo@tetraktys-cm.com>
2022-07-26 13:14:19 +0200

    styled places list

--- components/Places/PlaceItem.js
+++ components/Places/PlaceItem.js
@@ -1,18 +1,52 @@
 import { Image, Pressable, StyleSheet, View, Text } from "react-native";
+import { Colors } from "../../constants/colors";
 
 export const PlaceItem = ({place, onSelect}) => {
 
   return (
-    <Pressable onPress={onSelect}>
-      <Image source={{uri: place.imageUri}}/>
-      <View>
-        <Text>{place.title}</Text>
-        <Text>{place.address}</Text>
+    <Pressable onPress={onSelect} style={({pressed}) => [styles.item, pressed && styles.pressed]}>
+      <Image source={{uri: place.imageUri}} style={styles.image}/>
+      <View style={styles.info}>
+        <Text style={styles.title}>{place.title}</Text>
+        <Text style={styles.address}>{place.address}</Text>
       </View>
     </Pressable>
   );
 };
 
 const styles = StyleSheet.create({
-  // TODO
+  item: {
+    flexDirection: "row",
+    alignItems: "flex-start",
+    borderRadius: 8,
+    marginVertical: 12,
+    backgroundColor: Colors.primary500,
+    elevation: 2,
+    shadowColor: 'black',
+    shadowOffset: {width: 1, height: 1},
+    shadowOpacity: 0.15,
+    shadowRadius: 2,
+  },
+  pressed: {
+    opacity: 0.9,
+  },
+  image: {
+    flex: 1,
+    borderBottomLeftRadius: 8,
+    borderTopLeftRadius: 8,
+    height: 100,
+  },
+  info: {
+    flex: 2,
+    padding: 12,
+  },
+  title: {
+    fontWeight: "bold",
+    fontSize: 18,
+    color: Colors.gray700,
+  },
+  address: {
+    fontSize: 12,
+    color: Colors.gray700,
+  },
 });
