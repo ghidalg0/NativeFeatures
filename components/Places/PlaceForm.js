c518133f43626b49692ed6b3bbdbf8c22d7425fe
Guilhem Hidalgo <guilhem.hidalgo@tetraktys-cm.com>
2022-07-26 10:58:19 +0200

    added submit button

--- components/Places/PlaceForm.js
+++ components/Places/PlaceForm.js
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { ScrollView, StyleSheet, Text, TextInput, View } from "react-native";
 import { Colors } from "../../constants/colors";
+import { Button } from "../ui/Button";
 import { ImagePicker } from "./ImagePicker";
 import { LocationPicker } from "./LocationPicker";
 
@@ -13,6 +14,8 @@ export const PlaceForm = () => {
     setEnteredTitle(enteredText)
   };
 
+  const savePlaceHandler = () => {};
+
   return (
     <ScrollView style={styles.form}>
       <View>
@@ -21,6 +24,7 @@ export const PlaceForm = () => {
       </View>
       <ImagePicker />
       <LocationPicker />
+      <Button onPress={savePlaceHandler}>Add Place</Button>
     </ScrollView>
   );
 }
